d0a11bb132e8b860ad25fed9d284d6c3b8c4ce35
joverzhang <joverzh@gmail.com>
2021-10-13 00:25:33 +0800

    feat: Init project

---
+++ src/main/java/cn/edu/gdbtu/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.gdbtu.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jover Zhang
+ * @date 2021/10/12
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
