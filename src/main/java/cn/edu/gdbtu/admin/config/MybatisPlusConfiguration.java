cc1506a04610a691608e2dbbfe040b8c8bfc2749
joverzhang <joverzh@gmail.com>
2021-10-13 18:36:51 +0800

    fix: 分页缺少数据

---
+++ src/main/java/cn/edu/gdbtu/admin/config/MybatisPlusConfiguration.java
@@ -0,0 +1,23 @@
+package cn.edu.gdbtu.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jover Zhang
+ * @date 2021/10/13
+ */
+@Configuration
+public class MybatisPlusConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
