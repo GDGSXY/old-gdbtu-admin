07a18d2cc55c301a80aaf7f906f791ee85e2658a
joverzhang <joverzh@gmail.com>
2021-10-16 18:02:14 +0800

    feat: add GlobalExceptionHandler

---
+++ src/main/java/cn/edu/gdbtu/admin/config/web/GlobalExceptionConfiguration.java
@@ -0,0 +1,31 @@
+package cn.edu.gdbtu.admin.config.web;
+
+import cn.edu.gdbtu.admin.common.web.R;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * @author Jover Zhang
+ * @date 2021/10/16
+ */
+@Configuration
+@RestControllerAdvice
+public class GlobalExceptionConfiguration extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public R<Void> handleIllegalArgumentException(Exception exception) {
+        return R.fail(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus
+    public R<Void> handleExceptions(Exception exception) {
+        return R.fail(exception.getMessage());
+    }
+
+}
