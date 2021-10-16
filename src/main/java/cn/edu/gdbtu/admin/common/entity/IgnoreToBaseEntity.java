40ecdc0027d0d168019f005e89c362b1c19730c8
joverzhang <joverzh@gmail.com>
2021-10-17 00:23:33 +0800

    feat: add IgnoreToBaseEntity

---
+++ src/main/java/cn/edu/gdbtu/admin/common/entity/IgnoreToBaseEntity.java
@@ -0,0 +1,14 @@
+package cn.edu.gdbtu.admin.common.entity;
+
+import org.mapstruct.Mapping;
+
+/**
+ * @author Jover Zhang
+ * @date 2021/10/17
+ */
+@Mapping(target = "id", ignore = true)
+@Mapping(target = "gmtCreate", ignore = true)
+@Mapping(target = "gmtModified", ignore = true)
+@Mapping(target = "deleted", ignore = true)
+public @interface IgnoreToBaseEntity {
+}
