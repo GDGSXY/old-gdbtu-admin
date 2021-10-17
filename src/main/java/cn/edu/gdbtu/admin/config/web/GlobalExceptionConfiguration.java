e1b8871e01bce6cf9bf745ca1f81da417e6f5943
joverzhang <joverzh@gmail.com>
2021-10-18 01:18:34 +0800

    fix: GlobalException missing handler

--- src/main/java/cn/edu/gdbtu/admin/config/web/GlobalExceptionConfiguration.java
+++ src/main/java/cn/edu/gdbtu/admin/config/web/GlobalExceptionConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * @author Jover Zhang
@@ -14,7 +13,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  */
 @Configuration
 @RestControllerAdvice
-public class GlobalExceptionConfiguration extends ResponseEntityExceptionHandler {
+public class GlobalExceptionConfiguration {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
