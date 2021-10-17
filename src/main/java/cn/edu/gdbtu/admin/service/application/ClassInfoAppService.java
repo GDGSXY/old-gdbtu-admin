325cb25cd4f88fdd5aa7379ad1e485eb3fdf4d16
simpleLiving <1369058124@qq.com>
2021-10-18 00:32:00 +0800

    忘记导包

--- src/main/java/cn/edu/gdbtu/admin/service/application/ClassInfoAppService.java
+++ src/main/java/cn/edu/gdbtu/admin/service/application/ClassInfoAppService.java
@@ -1,6 +1,8 @@
 package cn.edu.gdbtu.admin.service.application;
 
 
+import cn.edu.gdbtu.admin.controller.cmd.CreateClassInfoCMD;
+
 public interface ClassInfoAppService {
     void createClassInfo(CreateClassInfoCMD cmd);
 }
